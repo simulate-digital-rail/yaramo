d577189c21cb8f0082973907481b3359708081c1
Benedikt Schenkel <benedikt.schenkel@student.hpi.de>
2022-12-06 11:54:28 +0100

    fix reference to geo_point in distance calculation

--- yaramo/geo_node.py
+++ yaramo/geo_node.py
@@ -10,11 +10,11 @@ class GeoNode(BaseElement):
         super().__init__(**kwargs)
         self.geo_point = GeoPoint(x,y)
     
-    def get_distance_to_other_geo_node(self, geo_node_b):
-        min_x = min(self.x, geo_node_b.x)
-        min_y = min(self.y, geo_node_b.y)
-        max_x = max(self.x, geo_node_b.x)
-        max_y = max(self.y, geo_node_b.y)
+    def get_distance_to_other_geo_node(self, geo_node_b: 'GeoNode'):
+        min_x = min(self.geo_point.x, geo_node_b.geo_point.x)
+        min_y = min(self.geo_point.y, geo_node_b.geo_point.y)
+        max_x = max(self.geo_point.x, geo_node_b.geo_point.x)
+        max_y = max(self.geo_point.y, geo_node_b.geo_point.y)
         return math.sqrt(math.pow(max_x - min_x, 2) + math.pow(max_y - min_y, 2))
 
 
