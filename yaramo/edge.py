7294d3851f14550adfc6d184a5de218e668b6935
Julian B <jeriox@users.noreply.github.com>
2022-12-06 14:51:55 +0100

    fix models for railway-route-generator use

--- yaramo/edge.py
+++ yaramo/edge.py
@@ -2,7 +2,6 @@ from typing import List
 from yaramo.base_element import BaseElement
 from yaramo.geo_node import GeoNode
 from yaramo.node import Node
-from yaramo.signal import Signal, SignalDirection
 
 
 class Edge(BaseElement):
@@ -40,14 +39,16 @@ class Edge(BaseElement):
         total_length += self.intermediate_geo_nodes[-1].get_distance_to_other_geo_node(self.node_b.geo_node)
         return total_length
     
-    def get_direction_based_on_nodes(self, node_a, node_b) -> SignalDirection:
+    def get_direction_based_on_nodes(self, node_a, node_b) -> "SignalDirection":
+        from yaramo.signal import SignalDirection
         if self.node_a.uuid == node_a.uuid and self.node_b.uuid == node_b.uuid:
             return SignalDirection.IN
         elif self.node_a.uuid == node_b.uuid and self.node_b.uuid == node_a.uuid:
             return SignalDirection.GEGEN
         return None
 
-    def get_signals_with_direction_in_order(self, direction) -> List[Signal]:
+    def get_signals_with_direction_in_order(self, direction) -> List["Signal"]:
+        from yaramo.signal import SignalDirection
         result: list[Signal] = []
         for signal in self.signals:
             if signal.direction == direction:
