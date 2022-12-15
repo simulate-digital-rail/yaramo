2f7f5baa5e6f66033a4e411aa7caea7ebe7e8cf3
Benedikt Schenkel <benedikt.schenkel@student.hpi.de>
2022-12-15 09:33:52 +0100

    export SignalKind

--- yaramo/model.py
+++ yaramo/model.py
@@ -1,4 +1,4 @@
-from yaramo.signal import Signal, SignalDirection, SignalFunction
+from yaramo.signal import Signal, SignalDirection, SignalFunction, SignalKind
 from yaramo.additional_signal import AdditionalSignalZs1, AdditionalSignalZs2, AdditionalSignalZs3
 from yaramo.node import Node
 from yaramo.geo_node import Wgs84GeoNode, DbrefGeoNode
