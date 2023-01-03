278b69993aefaacdd17ba7bb033dab9122867cff
Julian B <jeriox@users.noreply.github.com>
2023-01-03 16:06:44 +0100

    add conversion from WGS84 to Dbref

--- yaramo/geo_node.py
+++ yaramo/geo_node.py
@@ -12,6 +12,15 @@ class GeoNode(ABC, BaseElement):
     def get_distance_to_other_geo_node(self, geo_node_b: 'GeoNode'):
         pass
 
+    @abstractmethod
+    def to_wgs84(self) -> 'Wgs84GeoNode':
+        pass
+
+    @abstractmethod
+    def to_dbref(self) -> 'DbrefGeoNode':
+        pass
+
+
 class Wgs84GeoNode(GeoNode):
 
     def __init__(self, x, y, **kwargs):
@@ -20,6 +29,14 @@ class Wgs84GeoNode(GeoNode):
 
     def get_distance_to_other_geo_node(self, geo_node_b: 'Wgs84GeoNode'):
         return self.geo_point.get_distance_to_other_geo_point(geo_node_b.geo_point)
+
+    def to_wgs84(self) -> 'Wgs84GeoNode':
+        return self
+
+    def to_dbref(self) -> 'DbrefGeoNode':
+        geopoint = self.geo_point.to_dbref()
+        return DbrefGeoNode(geopoint.x, geopoint.y)
+
 
 class DbrefGeoNode(GeoNode):
 
@@ -30,3 +47,8 @@ class DbrefGeoNode(GeoNode):
     def get_distance_to_other_geo_node(self, geo_node_b: 'DbrefGeoNode'):
         return self.geo_point.get_distance_to_other_geo_point(geo_node_b.geo_point)
 
+    def to_wgs84(self) -> 'Wgs84GeoNode':
+        raise NotImplementedError
+
+    def to_dbref(self) -> 'DbrefGeoNode':
+        return self
