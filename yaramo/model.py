59d9d2fab50f4fc9f31419ad84166f71a66c7069
Julian Baumann <jeriox@users.noreply.github.com>
2023-01-24 16:22:08 +0100

    add CI (#22)
    * add CI
    
    * add CI
    
    * re-enable imports

---
+++ yaramo/model.py
@@ -0,0 +1,7 @@
+from yaramo.edge import Edge  # pylint: noqa
+from yaramo.geo_node import DistanceFunction, GeoNode  # pylint: noqa
+from yaramo.node import Node  # pylint: noqa
+from yaramo.route import Route  # pylint: noqa
+from yaramo.signal import Signal, SignalDirection, SignalFunction  # pylint: noqa
+from yaramo.topology import Topology  # pylint: noqa
+from yaramo.trip import Trip  # pylint: noqa
