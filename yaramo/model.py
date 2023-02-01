be5b6849536df2e5dfef2ee2bfb72c38a03720a1
Benedikt Schenkel <benedikt.schenkel@student.hpi.de>
2023-02-01 11:32:22 +0100

    export SignalKind

--- yaramo/model.py
+++ yaramo/model.py
@@ -2,6 +2,6 @@ from yaramo.edge import Edge  # pylint: noqa
 from yaramo.geo_node import GeoNode, Wgs84GeoNode, DbrefGeoNode  # pylint: noqa
 from yaramo.node import Node  # pylint: noqa
 from yaramo.route import Route  # pylint: noqa
-from yaramo.signal import Signal, SignalDirection, SignalFunction  # pylint: noqa
+from yaramo.signal import Signal, SignalDirection, SignalFunction, SignalKind  # pylint: noqa
 from yaramo.topology import Topology  # pylint: noqa
 from yaramo.trip import Trip  # pylint: noqa
