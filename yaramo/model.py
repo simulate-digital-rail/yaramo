00c4985f77735947cd73ff081cbd9988724b6bdb
Arne Boockmeyer <github@arneboockmeyer.de>
2023-03-13 13:58:11 +0100

    Export Node Connection Direction as part of the model

--- yaramo/model.py
+++ yaramo/model.py
@@ -1,6 +1,6 @@
 from yaramo.edge import Edge  # pylint: noqa
 from yaramo.geo_node import GeoNode, Wgs84GeoNode, DbrefGeoNode  # pylint: noqa
-from yaramo.node import Node  # pylint: noqa
+from yaramo.node import Node, NodeConnectionDirection  # pylint: noqa
 from yaramo.route import Route  # pylint: noqa
 from yaramo.signal import Signal, SignalDirection, SignalFunction, SignalKind  # pylint: noqa
 from yaramo.topology import Topology  # pylint: noqa
